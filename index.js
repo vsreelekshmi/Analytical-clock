923f13ba5b924f829f7fef259d85751f7c8b6543
vsreelekshmi <sreelekshmi1771996@gmail.com>
2022-08-17 17:19:38 +0530

    feat: Add clock face - 2 files modified

--- index.js
+++ index.js
@@ -14,7 +14,32 @@ drawClock();
 
 //draw the clock
 function drawClock() {
+  drawFace(ctx, radius);
+}
+
+function drawFace() {
+  var grad;
+
   ctx.arc(centerX, centerY, radius, 0, 2 * Math.PI);
+  ctx.fillStyle = 'white';
+  ctx.fill();
+
+  //Create a radial gradient
+  grad = ctx.createRadialGradient(0, 0, radius*.5, 0, 0, radius*5.05);
+  //3 color stops, corresponding with the inner, middle, and outer edge of the arc
+  grad.addColorStop(0, 'white');
+  grad.addColorStop(0, 'black');
+  grad.addColorStop(0, '#38074D');
+  //Define the gradient as the stroke style of the drawing object
+  ctx.strokeStyle = grad;
+  //Define the line width of the drawing object
+  ctx.lineWidth = radius*0.05;
+  //Draw the circle
+  ctx.stroke();
+
+  //Draw the clock center:
+  ctx.beginPath();
+  ctx.arc(centerX, centerY, radius*0.05, 0, 2*Math.PI);
   ctx.fillStyle = '#38074D';
   ctx.fill();
 }
